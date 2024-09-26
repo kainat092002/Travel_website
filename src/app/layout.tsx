a8318d95f847731f053be2988c16bef4be66a3cb
kainat092002 <kainatmujawar09@gmail.com>
2024-09-26 14:18:45 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel UI/UX App for Camping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+        <main className="relative overflow-hidden">
+        {children}
+        </main>
+        <Footer/>
+      </body>
+    </html>
+  );
+}
